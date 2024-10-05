96ceabb6f96e1af59b21af2eed0f9c722178fd38
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-10-05 22:58:10 +0300

    1.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,23 @@ import { NavLink } from "react-router-dom"
 import  css from "./App.module.css"
 import clsx from "clsx";
 
+import axios from "axios";
+
+const fetch = ()
+
+// const APIKEY = '8b10ee9ec2e80fb61717045f67c36d53';
+
+
+axios.get('https://api.themoviedb.org/3/search/movie/authentication', {
+  params: {
+    language: "en-US",
+    query: 
+  },
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YjEwZWU5ZWMyZTgwZmI2MTcxNzA0NWY2N2MzNmQ1MyIsIm5iZiI6MTcyODE1NTMwNS42ODcxNDIsInN1YiI6IjY2ZmY5YTY3NmZjNzRlNTc1NmY3ZjgzZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Aqs7zPXEzgvUC_il--d4RvQhYGDHW5J__eb9gkBUbZc'
+  },
+});
 
 
 const getNavLinkClass = ({ isActive }) => {
@@ -35,7 +52,7 @@ export default function App() {
         </li>
       </ul>
     </div>
-  );
+  ); 
 }
 
 
