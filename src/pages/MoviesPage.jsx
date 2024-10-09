f7288e52096f810d19febc1e80e54f956bd4c41d
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-10-09 16:15:51 +0300

    fix mistake

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,60 +1,3 @@
-// import { useState, useEffect } from "react";
-// import { useSearchParams } from "react-router-dom";
-// import { fetchRequest } from "../themoviedb-api";
-// import MovieList from "../components/MovieList/MovieList";
-
-// export default function MoviesPage() {
-//   const [query, setQuery] = useState("");
-//   const [movies, setMovies] = useState([]);
-//   const [searchParams, setSearchParams] = useSearchParams();
-
-//   useEffect(() => {
-//     const searchQuery = searchParams.get("query");
-//     if (searchQuery) {
-//       setQuery(searchQuery);
-//       fetchMovies(searchQuery);
-//     }
-//   }, [searchParams]);
-
-//   const fetchMovies = async (searchQuery) => {
-//     try {
-//       const res = await fetchRequest(`search/movie?query=${searchQuery}`);
-//       setMovies(res.data.results);
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   };
-
-//   const handleSearch = async (event) => {
-//     event.preventDefault();
-//     if (!query) return;
-
-//     await fetchMovies(query);
-//     setSearchParams({ query });
-//   };
-
-//   return (
-//     <div>
-//       <h1>Search Movies</h1>
-//       <form onSubmit={handleSearch}>
-//         <input
-//           type="text"
-//           value={query}
-//           onChange={(e) => setQuery(e.target.value)}
-//           placeholder="Enter movie"
-//         />
-//         <button type="submit">Search</button>
-//       </form>
-
-//       <MovieList
-//         movies={movies}
-//         from="movies"
-//         search={searchParams.toString()}
-//       />
-//     </div>
-//   );
-// }
-
 import { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import { fetchRequest } from "../themoviedb-api";
@@ -82,11 +25,8 @@ export default function MoviesPage() {
     }
   };
 
-  const handleSearch = async (e) => {
-    e.preventDefault();
-    searchParams.set("owner", e.target.elements.owner.value);
-    setSearchParams(searchParams);
-
+  const handleSearch = async (event) => {
+    event.preventDefault();
     if (!query) return;
 
     await fetchMovies(query);
@@ -99,7 +39,6 @@ export default function MoviesPage() {
       <form onSubmit={handleSearch}>
         <input
           type="text"
-          name="owner"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           placeholder="Enter movie"
